14dffe212a73938c0a53a432bbb67542ad4f1846
cccin20 <cin.sur11@gmail.com>
2026-04-26 15:29:14 +0800

    final lab-act4 auth routes

--- surla-client/src/App.jsx
+++ surla-client/src/App.jsx
@@ -1,20 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-// HomePage Structure
+// Layouts
 import Layout from './layouts/Layout';
+import AuthLayout from './layouts/AuthLayout';
+
+// Landing Pages
 import ArticlePage from './pages/LandingPages/ArticlePage';
 import HomePage from './pages/LandingPages/HomePage';
 import AboutPage from './pages/LandingPages/AboutPage';
 import ArticleListPage from './pages/LandingPages/ArticleListPage';
 
-import AuthLayout from './layouts/AuthLayout';
+// Auth Pages
 import SignInPage from './pages/AuthPages/SignInPage';
 import SignUpPage from './pages/AuthPages/SignUpPage';
-
-import DashLayout from './layouts/DashLayout';
-import DashboardPage from './pages/DashboardPages/DashboardPage';
-import ReportsPage from './pages/DashboardPages/ReportsPage';
-import UsersPage from './pages/DashboardPages/UsersPage';
 
 import NotFoundPage from './pages/NotFoundPage';
 
@@ -43,49 +41,25 @@ const routes = [
     ],
   },
   {
-    path: "auth/",
+    path: 'auth',
     element: <AuthLayout />,
     errorElement: <NotFoundPage />,
     children: [
       {
-        path: "signin",
+        path: 'signin',
         element: <SignInPage />,
       },
       {
-        path: "signup",
+        path: 'signup',
         element: <SignUpPage />,
       },
     ],
-  },
-  {
-    path: "dashboard/",
-    element: <DashLayout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: "",
-        element: <DashboardPage />,
-      },
-      {
-        path: "reports",
-        element: <ReportsPage />,  
-      },
-      {
-        path: "users",
-        element: <UsersPage />,
-      }
-    ],
-  },
-];
+  },];
 
 const router = createBrowserRouter(routes);
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
