68794b61d10bc499f4bb8f2bc0a824879d88d6d9
cccin20 <cin.sur11@gmail.com>
2026-04-26 15:23:33 +0800

    final lab-act3 correct routes

--- surla-client/src/App.jsx
+++ surla-client/src/App.jsx
@@ -1,20 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-// HomePage Structure
+// Layout
 import Layout from './layouts/Layout';
+
+// Pages
 import ArticlePage from './pages/LandingPages/ArticlePage';
 import HomePage from './pages/LandingPages/HomePage';
 import AboutPage from './pages/LandingPages/AboutPage';
 import ArticleListPage from './pages/LandingPages/ArticleListPage';
-
-import AuthLayout from './layouts/AuthLayout';
-import SignInPage from './pages/AuthPages/SignInPage';
-import SignUpPage from './pages/AuthPages/SignUpPage';
-
-import DashLayout from './layouts/DashLayout';
-import DashboardPage from './pages/DashboardPages/DashboardPage';
-import ReportsPage from './pages/DashboardPages/ReportsPage';
-import UsersPage from './pages/DashboardPages/UsersPage';
 
 import NotFoundPage from './pages/NotFoundPage';
 
@@ -41,51 +34,12 @@ const routes = [
         element: <ArticlePage />,
       },
     ],
-  },
-  {
-    path: "auth/",
-    element: <AuthLayout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: "signin",
-        element: <SignInPage />,
-      },
-      {
-        path: "signup",
-        element: <SignUpPage />,
-      },
-    ],
-  },
-  {
-    path: "dashboard/",
-    element: <DashLayout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: "",
-        element: <DashboardPage />,
-      },
-      {
-        path: "reports",
-        element: <ReportsPage />,  
-      },
-      {
-        path: "users",
-        element: <UsersPage />,
-      }
-    ],
-  },
-];
+  },];
 
 const router = createBrowserRouter(routes);
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
